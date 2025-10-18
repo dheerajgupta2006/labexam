f6ee0ce576f9d3aef1552d3e368d04d221701ed7
dheerajgupta006 <dheerajguptavarada999@gmail.com>
2025-10-18 10:41:01 +0530

    fifith

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/labexam/', // This MUST match your repository name for proper routing on GitHub Pages
   plugins: [react()],
-})
+});
